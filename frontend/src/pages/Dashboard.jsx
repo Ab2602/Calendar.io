09bf3588542ef2f64d3555357fbd4768579f417e
Abhinav Tripathi <76097113+Ab2602@users.noreply.github.com>
2024-12-07 14:26:58 +0530

    Update Dashboard.jsx

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -28,7 +28,7 @@ const Dashboard = () => {
     const fetchUser = async () => {
       try {
         const response = await fetch(
-          `${import.meta.env.VITE_BACKEND_URL}/auth/current-user`,
+          `https://calendar-io-psi.vercel.app//auth/current-user`,
           { credentials: "include" }
         );
         const data = await response.json();
